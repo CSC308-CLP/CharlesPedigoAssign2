e6ff32272ec824c2119a148c8b648806155b6d33
Charles Lucas Pedigo <charles_pedigo@mymail.eku.edu>
2026-02-17 20:26:32 -0500

    Initial Commit

---
+++ CharlesPedigoAssign2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CharlesPedigoAssign2
+//
+//  Created by Pedigo, Charles L. on 2/17/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
